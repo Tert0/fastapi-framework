e2201905faa0489db2d193ee3929089220ddb088
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-06-26 10:20:12 +0200

    Added README.md

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as file:
     requirements = [line.strip() for line in file.readlines()]
 
+with open("README.md", "r") as file:
+    readme = file.read()
+
 setup(
     name='fastapi-framework',
     packages=find_packages(),
     version='0.1.0',
     description='A Fastapi Framework',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author='Tert0',
     license='MIT',
-    install_requires=requirements
+    install_requires=requirements,
+    extra_requires=[
+        "mkdocs-material"
+    ],
 )
