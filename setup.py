912f3c6413585073468c7f28c89ffe3f88704c33
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-06-27 11:49:38 +0200

    Fixed README Reader for Python Package

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("requirements.txt", "r") as file:
     requirements = [line.strip() for line in file.readlines()]
 
 with open("README.md", "r") as file:
-    readme = file.read()
+    readme = file.read().replace("[ ]", "❌").replace("[x]", "✅")
 
 setup(
     name="fastapi-framework",
     packages=find_packages(),
-    version="0.9.4",
+    version="0.9.4.1",
     description="A Fastapi Framework",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -17,4 +17,6 @@ setup(
     license="MIT",
     install_requires=requirements,
     extra_requires=["mkdocs-material"],
+    url="https://github.com/Tert0/fastapi-framework",
+    python_requires=">=3.6",
 )
