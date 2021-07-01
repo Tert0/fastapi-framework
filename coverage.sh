9537b4d9d0407cfe800c5211ddffddc3f36c112c
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-01 23:03:16 +0200

    Fixed Coverage Script

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-coverage run --source=fastapi_framework -m unittest discover -v tests
+
+if coverage run --source=fastapi_framework -m unittest discover -v tests
+then
+    coverage ${1:-xml}
+    coverage report
+    coverage erase
+else
+    coverage erase
+    exit 1
+fi
