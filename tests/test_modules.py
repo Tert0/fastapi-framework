ea5498501f1d7258a8245866e4177a518642b3c8
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-02 17:39:24 +0200

    Added Modules Unit Test

---
+++ tests/test_modules.py
@@ -0,0 +1,19 @@
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import AsyncMock, patch
+
+from fastapi_framework.modules import check_dependencies
+from fastapi_framework import modules
+
+
+class TestModules(IsolatedAsyncioTestCase):
+    @patch.object(modules, "disabled_modules", [])
+    async def test_check_dependencies_no_disabled(self):
+        check_dependencies()
+
+    @patch.object(modules, "disabled_modules", ["redis"])
+    async def test_check_dependencies_redis_disabled(self):
+        self.assertRaises(Exception, check_dependencies)
+
+    @patch.object(modules, "disabled_modules", ["redis", "rate_limit", "jwt_auth"])
+    async def test_check_dependencies_disabled_all_depends_on_redis(self):
+        check_dependencies()
