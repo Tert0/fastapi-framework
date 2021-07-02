4a4ee77fe8a41e875674a953aa4121f8f4b75fdb
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-02 17:53:18 +0200

    Improved Logger Test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,7 +1,8 @@
 import logging
+import sys
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
-from fastapi_framework.logger import logging_formatter, get_logger
+from fastapi_framework.logger import logging_formatter, get_logger, logging_handler
 
 
 class LoggerTest(TestCase):
@@ -9,11 +10,21 @@ class LoggerTest(TestCase):
         self.assertIsInstance(logging_formatter, logging.Formatter)
         self.assertEqual("[%(asctime)s] [%(levelname)s] %(message)s", logging_formatter._fmt)
 
+    @patch("fastapi_framework.logger.LOG_LEVEL")
+    @patch("fastapi_framework.logger.logging_handler")
     @patch("logging.getLogger")
-    def test_get_logger(self, getLogger_patch: MagicMock):
-        name = "logger_name"
+    def test_get_logger(self, getLogger_patch: MagicMock, logging_handler_patch: MagicMock, log_level_patch: MagicMock):
+        name = MagicMock()
 
         result = get_logger(name)
 
         getLogger_patch.assert_called_with(name)
         self.assertEqual(result, getLogger_patch())
+        getLogger_patch().addHandler.assert_called_with(logging_handler_patch)
+        getLogger_patch().setLevel.assert_called_with(log_level_patch.upper())
+
+    def test_logging_handler(self):
+        self.assertIsInstance(logging_handler, logging.StreamHandler)
+        self.assertEqual(logging_handler.stream, sys.stdout)
+        self.assertEqual(logging_handler.formatter, logging_formatter)
+
