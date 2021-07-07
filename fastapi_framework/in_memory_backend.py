e2745e91ecd3fa863f2241279ad114dfca47f6cb
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-07 17:24:54 +0200

    Added InMemoryBackend

---
+++ fastapi_framework/in_memory_backend.py
@@ -0,0 +1,136 @@
+import time
+from typing import Dict, Any, Optional
+from abc import ABC
+
+
+class InMemoryBackend(ABC):
+    async def set(self, key: str, value, expire: int = 0, pexpire: int = 0, exists=None):
+        """Set Key to Value"""
+
+    async def get(self, key: str):
+        """Get Value from Key"""
+
+    async def pttl(self, key: str) -> int:
+        """Get PTTL from a Key"""
+
+    async def ttl(self, key: str) -> int:
+        """Get TTL from a Key"""
+
+    async def pexpire(self, key: str, pexpire: int) -> bool:
+        """Sets and PTTL for a Key"""
+
+    async def expire(self, key: str, expire: int) -> bool:
+        """Sets and TTL for a Key"""
+
+    async def incr(self, key: str) -> int:
+        """Increases an Int Key"""
+
+    async def decr(self, key: str) -> int:
+        """Decreases an Int Key"""
+
+    async def delete(self, key: str):
+        """Delete value of a Key"""
+
+
+class RAMBackendItem:
+    value: Any
+    pexpire: int
+    timestamp: int
+
+    def __init__(self, value: Any, pexpire: int):
+        self.value = value
+        self.pexpire = pexpire
+        self.timestamp = int(time.time() * 1000)
+
+
+class RAMBackend(InMemoryBackend):
+    data: Dict[str, RAMBackendItem] = {}
+    SET_IF_NOT_EXIST = 'SET_IF_NOT_EXIST'  # NX
+    SET_IF_EXIST = 'SET_IF_EXIST'  # XX
+
+    async def _check_key_expire(self, key: str, item: RAMBackendItem):
+        timestamp = int(time.time() * 1000)
+        if item.pexpire > 0 and (item.timestamp + item.pexpire) <= timestamp:
+            self.data.pop(key)
+            return False
+        return True
+
+    async def set(self, key: str, value, expire: int = 0, pexpire: int = 0, exists=None):
+        if exists == self.SET_IF_NOT_EXIST:
+            if key in self.data:
+                return
+        elif exists == self.SET_IF_EXIST:
+            if key not in self.data:
+                return
+        elif exists is None:
+            pass
+        else:
+            raise Exception("Wrong Params")
+        self.data[key] = RAMBackendItem(value, pexpire + (expire * 1000))
+
+    async def get(self, key: str):
+        item: Optional[RAMBackendItem] = self.data.get(key)
+        if not item:
+            return None
+        if not await self._check_key_expire(key, item):
+            return None
+        return item.value
+
+    async def pttl(self, key: str) -> int:
+        item: Optional[RAMBackendItem] = self.data.get(key)
+        timestamp = int(time.time() * 1000)
+        if not item:
+            return -2
+        if not await self._check_key_expire(key, item):
+            return -1
+        if item.pexpire == 0:
+            return -1
+        return (item.pexpire + item.timestamp) - timestamp
+
+    async def ttl(self, key: str) -> int:
+        pttl = await self.pttl(key)
+        if pttl >= 0:
+            return pttl // 1000
+        return pttl
+
+    async def pexpire(self, key: str, pexpire: int) -> bool:
+        item: Optional[RAMBackendItem] = self.data.get(key)
+        timestamp = int(time.time() * 1000)
+        if not item:
+            return False
+        if not await self._check_key_expire(key, item):
+            return False
+        item.timestamp = timestamp
+        item.pexpire = pexpire
+        self.data[key] = item
+        return True
+
+    async def expire(self, key: str, expire: int) -> bool:
+        return await self.pexpire(key, expire * 1000)
+
+    async def incr(self, key: str) -> int:
+        item: Optional[RAMBackendItem] = self.data.get(key)
+        if not item:
+            await self.set(key, 1)
+            return 1
+        if not isinstance(item.value, int):
+            raise Exception("Value must be a Int")
+        item.value += 1
+        self.data[key] = item
+        return item.value
+
+    async def decr(self, key: str) -> int:
+        item: Optional[RAMBackendItem] = self.data.get(key)
+        if not item:
+            await self.set(key, -1)
+            return -1
+        if not isinstance(item.value, int):
+            raise Exception("Value must be a Int")
+        item.value -= 1
+        self.data[key] = item
+        return item.value
+
+    async def delete(self, key: str):
+        if key not in self.data:
+            return
+        del self.data[key]
---
+++ tests/test_in_memory_backend.py
@@ -0,0 +1,149 @@
+import asyncio
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import patch, AsyncMock
+
+from fastapi_framework.in_memory_backend import RAMBackend
+
+ram_backend = RAMBackend()
+
+
+class TestInMemoryBackend(IsolatedAsyncioTestCase):
+    async def test_init_ram_backend(self):
+        RAMBackend()
+
+    async def test_set_value(self):
+        await ram_backend.set("test_set_value", "test_value")
+        self.assertTrue("test_set_value" in ram_backend.data)
+        self.assertEqual(ram_backend.data["test_set_value"].value, "test_value")
+
+    async def test_get_value(self):
+        await ram_backend.set("test_get_value", "test_value")
+        self.assertEqual(await ram_backend.get("test_get_value"), "test_value")
+
+    async def test_set_value_wrong_params(self):
+        with self.assertRaises(Exception):
+            await ram_backend.set("test_set_value_wrong_params", "test_value", exists="WRONG")
+
+    async def test_set_value_with_pttl(self):
+        await ram_backend.set("test_set_value_with_pttl", "test_value", pexpire=2000)
+        self.assertTrue("test_set_value_with_pttl" in ram_backend.data)
+        pexpire = ram_backend.data["test_set_value_with_pttl"].pexpire
+        self.assertIsInstance(pexpire, int)
+        self.assertTrue(pexpire <= 2000)
+
+    async def test_set_value_with_ttl(self):
+        await ram_backend.set("test_set_value_with_ttl", "test_value", expire=10)
+        self.assertTrue("test_set_value_with_ttl" in ram_backend.data)
+        expire = int(ram_backend.data["test_set_value_with_ttl"].pexpire / 1000)
+        self.assertIsInstance(expire, int)
+        self.assertTrue(expire <= 10)
+
+    async def test_get_pttl(self):
+        await ram_backend.set("test_get_pttl", "test_value", expire=10)
+        self.assertTrue("test_get_pttl" in ram_backend.data)
+        pexpire = ram_backend.data["test_get_pttl"].pexpire
+        pexpire_getter = await ram_backend.pttl("test_get_pttl")
+        self.assertIsInstance(pexpire, int)
+        self.assertIsInstance(pexpire, int)
+        self.assertTrue(pexpire_getter <= pexpire)
+
+    async def test_get_pttl_key_dont_exists(self):
+        self.assertEqual(await ram_backend.pttl("this_key_doesn't_exists"), -2)
+
+    async def test_get_pttl_with_expired_key(self):
+        await ram_backend.set("test_get_pttl_with_expired_key", "test_value", pexpire=1)
+        await asyncio.sleep(0.01)
+        self.assertEqual(await ram_backend.pttl("test_get_pttl_with_expired_key"), -1)
+
+    async def test_get_value_not_existing(self):
+        self.assertEqual(await ram_backend.get("this_key_doesn't_exists"), None)
+
+    async def test_pexpire(self):
+        await ram_backend.set("test_pexpire", "test_value")
+        await ram_backend.pexpire("test_pexpire", 1000)
+        self.assertTrue("test_pexpire" in ram_backend.data)
+        self.assertEqual(ram_backend.data["test_pexpire"].pexpire, 1000)
+
+    async def test_pexpire_dont_exsist(self):
+        self.assertEqual(await ram_backend.pexpire("this_key_doesn't_exists", 1), False)
+
+    @patch("fastapi_framework.in_memory_backend.RAMBackend.pexpire")
+    async def test_expire(self, pexpire_patch: AsyncMock):
+        await ram_backend.expire("test", 15)
+        pexpire_patch.assert_called_with("test", 15000)
+
+    async def test_increase(self):
+        await ram_backend.set("test_increase", 1)
+        for _ in range(10):
+            await ram_backend.incr("test_increase")
+        self.assertEqual(await ram_backend.get("test_increase"), 11)
+
+    async def test_decrease(self):
+        await ram_backend.set("test_decrease", 11)
+        for _ in range(10):
+            await ram_backend.decr("test_decrease")
+        self.assertEqual(await ram_backend.get("test_decrease"), 1)
+
+    async def test_increase_dont_exists(self):
+        for _ in range(10):
+            await ram_backend.incr("test_increase_dont_exists")
+        self.assertEqual(await ram_backend.get("test_increase_dont_exists"), 10)
+
+    async def test_decrease_dont_exists(self):
+        for _ in range(10):
+            await ram_backend.decr("test_decrease_dont_exists")
+        self.assertEqual(await ram_backend.get("test_decrease_dont_exists"), -10)
+
+    async def test_increase_with_string(self):
+        await ram_backend.set("test_increase_with_string", "hello")
+        with self.assertRaises(Exception):
+            await ram_backend.incr("test_increase_with_string")
+
+    async def test_decrease_with_string(self):
+        await ram_backend.set("test_decrease_with_string", "hello")
+        with self.assertRaises(Exception):
+            await ram_backend.decr("test_decrease_with_string")
+
+    async def test_get_expired_key(self):
+        await ram_backend.set("test_get_expired_key", "test_value", pexpire=1)
+        await asyncio.sleep(0.001)
+        self.assertEqual(await ram_backend.get("test_get_expired_key"), None)
+
+    async def test_get_ttl(self):
+        await ram_backend.set("test_get_ttl", "test_value", expire=10)
+        self.assertIsInstance(await ram_backend.ttl("test_get_ttl"), int)
+        print(await ram_backend.ttl("test_get_ttl"))
+        self.assertTrue(await ram_backend.ttl("test_get_ttl") <= 10)
+
+    async def test_get_ttl_not_expiring(self):
+        await ram_backend.set("test_get_ttl_not_expiring", "test_value")
+        result = await ram_backend.ttl("test_get_ttl_not_expiring")
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, -1)
+
+    async def test_pexpire_with_expired_key(self):
+        await ram_backend.set("test_pexpire_with_expired_key", "test_value", pexpire=1)
+        await asyncio.sleep(0.001)
+        result = await ram_backend.pexpire("test_pexpire_with_expired_key", 1)
+        self.assertIsInstance(result, bool)
+        self.assertEqual(result, False)
+
+    async def test_delete(self):
+        await ram_backend.set("test_delete", "test_value")
+        self.assertEqual(await ram_backend.get("test_delete"), "test_value")
+        await ram_backend.delete("test_delete")
+        self.assertEqual(await ram_backend.get("test_delete"), None)
+
+    async def test_delete_dont_exists(self):
+        await ram_backend.delete("test_delete_dont_exists")
+
+    async def test_set_if_not_exists_and_set_if_exists(self):
+        await ram_backend.set("test_set_if_not_exists", "1", exists=ram_backend.SET_IF_NOT_EXIST)
+        self.assertEqual(await ram_backend.get("test_set_if_not_exists"), "1")
+        await ram_backend.set("test_set_if_not_exists", "2", exists=ram_backend.SET_IF_NOT_EXIST)
+        self.assertEqual(await ram_backend.get("test_set_if_not_exists"), "1")
+        await ram_backend.set("test_set_if_not_exists", "3", exists=ram_backend.SET_IF_EXIST)
+        self.assertEqual(await ram_backend.get("test_set_if_not_exists"), "3")
+        del ram_backend.data["test_set_if_not_exists"]
+        await ram_backend.set("test_set_if_not_exists", "4", exists=ram_backend.SET_IF_EXIST)
+        self.assertEqual(await ram_backend.get("test_set_if_not_exists"), None)
