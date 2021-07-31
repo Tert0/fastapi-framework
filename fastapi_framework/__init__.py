7d484a59d3a417a90b552a3aa83597f57be9a93b
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-31 10:39:12 +0200

    v1.4.6.2

--- fastapi_framework/__init__.py
+++ fastapi_framework/__init__.py
@@ -1,6 +1,6 @@
 """A FastAPI Framework for things like Database, Redis, Logging, JWT Authentication and Rate Limits"""
 
-__version__ = "1.4.6.1"
+__version__ = "1.4.6.2"
 
 from .modules import check_dependencies, disabled_modules
 
