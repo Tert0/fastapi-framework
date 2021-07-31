cc8eec08f083602cbb8fb9cb3cb09518d3dbffb1
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-31 09:29:19 +0200

    Updated Tests for Config Module

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
+from typing import Any
 from unittest import TestCase
-from unittest.mock import patch, MagicMock, mock_open
-from fastapi_framework.config import Config
+from unittest.mock import patch, mock_open
+from fastapi_framework import Config, ConfigField, config
 
 
 class TestConfig(TestCase):
@@ -12,8 +13,8 @@ class TestConfig(TestCase):
     @patch("builtins.open", mock_open(read_data=test_yaml_config_data))
     def test_yaml_config(self):
         class MyConfig(Config):
-            some_data: str = "test"
-            some_other_data: str = "default value"
+            some_data: str = ConfigField("some_data")
+            some_other_data: str = ConfigField(default="default value")
 
         self.assertEqual(MyConfig.some_data, "a value")
         self.assertEqual(MyConfig.some_other_data, "default value")
@@ -24,8 +25,8 @@ class TestConfig(TestCase):
             CONFIG_PATH = "config.json"
             CONFIG_TYPE = "json"
 
-            some_data: str
-            some_other_data: str = "default value"
+            some_data: str = ConfigField("some_data")
+            some_other_data: str = ConfigField(default="default value")
 
         self.assertEqual(MyConfig.some_data, "a value")
         self.assertEqual(MyConfig.some_other_data, "default value")
@@ -36,8 +37,8 @@ class TestConfig(TestCase):
             CONFIG_PATH = "config.toml"
             CONFIG_TYPE = "toml"
 
-            some_data: str
-            some_other_data: str = "default value"
+            some_data: str = ConfigField("some_data")
+            some_other_data: str = ConfigField(default="default value")
 
         self.assertEqual(MyConfig.some_data, "a value")
         self.assertEqual(MyConfig.some_other_data, "default value")
@@ -48,8 +49,8 @@ class TestConfig(TestCase):
             CONFIG_PATH: str = "config.yaml"
             CONFIG_TYPE: str = "yaml"
 
-            some_data: str
-            some_other_data: str = "default value"
+            some_data: str = ConfigField("some_data")
+            some_other_data: str = ConfigField(default="default value")
 
         self.assertEqual(MyConfig.some_data, "a value")
         self.assertEqual(MyConfig.some_other_data, "default value")
@@ -58,17 +59,32 @@ class TestConfig(TestCase):
     def test_config_type_dont_exists(self):
         with self.assertRaises(Exception):
 
-            class MyConfig(Config):
+            class _(Config):
                 CONFIG_TYPE = "type doesn't exists"
 
-                some_data: str
-                some_other_data: str = "default value"
+                some_data: str = ConfigField()
+                some_other_data: str = ConfigField(default="default value")
 
     @patch("builtins.open", mock_open(read_data=test_yaml_config_with_typing_data))
     def test_yaml_config_with_typing(self):
         from typing import List
 
         class MyConfig(Config):
-            items: List
+            items: List = ConfigField()
 
         self.assertEqual(MyConfig.items, ["an item", "an other item"])
+
+    @patch("builtins.open", mock_open(read_data=test_yaml_config_data))
+    def test_yaml_config_with_type_any(self):
+        class _(Config):
+            some_data: Any = ConfigField("some_data")
+            some_other_data: Any = ConfigField(default="default value")
+
+    @patch.object(config, "disabled_modules", ["config"])
+    def test_config_disabled(self):
+        class MyConfig(Config):
+            some_data: str = ConfigField("some_data")
+            some_other_data: str = ConfigField(default="default value")
+
+        self.assertEqual(MyConfig.some_data, None)
+        self.assertEqual(MyConfig.some_other_data, None)
